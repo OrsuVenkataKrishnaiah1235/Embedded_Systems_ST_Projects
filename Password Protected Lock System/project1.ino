beee55c03c8f638202038e7109428c6604597dd2
Orsu Venkata Krishnaiah <119290590+OrsuVenkataKrishnaiah1235@users.noreply.github.com>
2023-01-12 20:36:20 +0530

    Add files via upload
    Signed-off-by: Orsu Venkata Krishnaiah <119290590+OrsuVenkataKrishnaiah1235@users.noreply.github.com>

---
+++ Password Protected Lock System/project1.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+int buzzer=11;
+int Red_Led=10;
+int Green_Led=12;
+int a[4]={1,2,3,4};
+
+LiquidCrystal_I2C lcd(0x20,16,2);
+void setup()
+{
+for(int i=0;i<=9;i++)
+{
+  pinMode(i,INPUT);
+}
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+}
+
+void loop()
+{
+if(digitalRead(0)==a[0])
+{
+  if((digitalRead(1)==a[1])
+  {
+  if(digitalRead(2)==a[2])
+  {
+    if(digitalRead(3)==a[3])
+    {
+      lcd.print("Welcome");
+      digitalWrite(12,HIGH);
+      delay(1000);
+    }
+  }
+  }
+}
+else
+{
+  lcd.print("Contact Manager/Retype the password");
+  digitalWrite(10,HIGH);
+  digitalWrite(12,HIGH);
+  delay(1000);
+}
+}

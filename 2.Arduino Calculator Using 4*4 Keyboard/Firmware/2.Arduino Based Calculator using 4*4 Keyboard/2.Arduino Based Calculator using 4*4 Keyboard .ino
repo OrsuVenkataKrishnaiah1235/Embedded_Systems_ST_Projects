f61113b3a87aeb8886e5ef36ad6adf567cc9ec93
Orsu Venkata Krishnaiah <119290590+OrsuVenkataKrishnaiah1235@users.noreply.github.com>
2023-01-18 20:26:54 +0530

    Update 2.Arduino Based Calculator using 4*4 Keyboard .ino
    Signed-off-by: Orsu Venkata Krishnaiah <119290590+OrsuVenkataKrishnaiah1235@users.noreply.github.com>

--- 2.Arduino Calculator Using 4*4 Keyboard/Firmware/2.Arduino Based Calculator using 4*4 Keyboard/2.Arduino Based Calculator using 4*4 Keyboard .ino
+++ 2.Arduino Calculator Using 4*4 Keyboard/Firmware/2.Arduino Based Calculator using 4*4 Keyboard/2.Arduino Based Calculator using 4*4 Keyboard .ino
@@ -2,127 +2,61 @@
 #include <Keypad.h> 
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Four columns
-
+long Num1,Num2,Number;//Num1 and Num2 Upto  2 Decimal points only allowed
+char key,action;
+boolean result = false;
 
 // Defining  the Keymap
-
-char keys[ROWS][COLS] = {
-
-
-  {'7','8','9','D'},
-
-
-  {'4','5','6','C'},
-
-
-  {'1','2','3','B'},
-
-
-  {'*','0','#','A'}
-
-
-};
-
-
+char keys[ROWS][COLS] = {{'7','8','9','D'},{'4','5','6','C'},{'1','2','3','B'},{'*','0','#','A'}};
 byte rowPins[ROWS] = { 0, 1, 2, 3 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-
 byte colPins[COLS] = { 4, 5, 6, 7 }; // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-
-
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //  Create the Keypad Constructor
 //Keypad(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols);
-
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //  Create the Keypad Constructor
 
 const int rs = 8, en = 9, d4 = 10, d5 = 11, d6 = 12, d7 = 13; //Pins to which LCD is connected
-
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
-
- long Num1,Num2,Number;//Num1 and Num2 Upto  2 Decimal points only allowed
-
- char key,action;
-
- boolean result = false;
-
- 
-
-void setup() {
-
-  lcd.begin(16, 2); //We are using a 16*2 LCD display
-
-  lcd.print("2 Digits Calculator"); //Display a intro message
-
-  lcd.setCursor(0, 1);   // set the cursor to column 0, line 1
-
-
-   delay(2000); //Wait for display to show info
-
-    lcd.clear(); //Then clean it
-
+void setup() 
+{
+lcd.begin(16, 2); //We are using a 16*2 LCD display
+lcd.print("2 Digits Calculator"); //Display a intro message
+lcd.setCursor(0, 1);   // set the cursor to column 0, line 1
+delay(2000); //Wait for display to show info
+lcd.clear(); //Then clean it
 }
-void loop() {
 
-  
-
+void loop() 
+{
 key = kpd.getKey(); //storing pressed key value in a char getKey() is a Instance Method 
-
-
 if (key!=NO_KEY)
-
 DetectButtons();
-
-
 if (result==true)
-
 CalculateResult();
-
-
 DisplayResult();   
-
 }
-
-
-
-
 
 void DetectButtons()
-
 { 
-
-     lcd.clear(); //Blank LCD
-  //For Key 0
-  
-    if (key == '0')
-
-    {
-      Serial.println ("Button 0"); //Button 0 is Pressed
-
-    if (Number==0)
-
-    Number=0;
-
-    else
-
-    Number = (Number*10) + 0; //Pressed twice
-
-    }
-    //For Key 1
-     if (key == '1') //If Button 1 is pressed
-
-    {
-       Serial.println ("Button 1"); 
-
-    if (Number==0)
-
-    Number=1;
-
-    else
-
-    Number = (Number*10) + 1; //Pressed twice 11
-
-    }
-  //For Key 2
+lcd.clear(); //Blank LCD
+//For Key 0
+if (key == '0')
+{
+Serial.println ("Button 0"); //Button 0 is Pressed
+if (Number==0)
+  Number=0;
+else
+  Number = (Number*10) + 0; //Pressed twice
+}
+//For Key 1
+if (key == '1') //If Button 1 is pressed
+{
+Serial.println ("Button 1"); 
+if (Number==0)
+  Number=1;
+else
+  Number = (Number*10) + 1; //Pressed twice 11
+}
+ //For Key 2
 
      if (key == '2') //Button 2 is Pressed
 
